3c60c6cc218d0c3ce774cd613716bd1d407a1f46
Luis Lenta <luislenta@gmail.com>
2019-10-05 13:21:26 -0300

    Empiezo ejercicio de DMA DAC

---
+++ workspace/TeoricoClase9_DMADAC/Debug/src/main.d
@@ -0,0 +1,37 @@
+src/main.o src/main.d: ../src/main.c \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/LPC17xx.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/core_cm3.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/lpc17xx.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpdma.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h \
+ C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_systick.h
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/core_cm3.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\inc/lpc17xx.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpdma.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h:
+
+C:\Users\Luis\ Lenta\Documents\MCUXpressoIDE_11.0.0_2516\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_systick.h:
--- workspace/TeoricoClase9_DMADAC/src/main.c
+++ workspace/TeoricoClase9_DMADAC/src/main.c
@@ -16,29 +16,98 @@
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpdma.h"
 #include "lpc17xx_dac.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_systick.h"
 
 #define RAMPA 				100
 #define EXTRA				42
 #define NUMERO_MUESTRAS		142
 #define FRECUENCIA 			100
-#define CLOCK_DAC_MHZ 		25  	//CLK divided by 4
+#define CLOCK_DAC 			25000000  	//CLK divided by 4
 
-void init (void);
 void configDAC(void);
 void configGPDMA(void);
 void configPINSEL(void);
-
+void init (void);
+void configSYSTICK(void);
 void crearLista (void);
 
+uint32_t TABLA_DAC[NUMERO_MUESTRAS];
 int main(void) {
 
-    // TODO: insert code here
+	crearLista();
+	init();
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
+	volatile static int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
     while(1) {
         i++ ;
     }
     return 0 ;
+}
+void configDAC (void) {
+	DAC_CONVERTER_CFG_Type dac;
+
+	dac.CNT_ENA = SET; 				//Time out counter ENABLE
+	dac.DMA_ENA = SET; 				//DMA access ENABLE
+
+	DAC_ConfigDAConverterControl(LPC_DAC, &dac);
+
+	uint32_t tmp = CLOCK_DAC / (FRECUENCIA * NUMERO_MUESTRAS);
+	DAC_SetDMATimeOut(LPC_DAC, tmp);								//Tiempo en el cual DMA actualiza valor a convertir en DAC
+
+	DAC_Init(LPC_DAC);
+}
+void configGPDMA(void){
+	GPDMA_Channel_CFG_Type  channelGPDMA;
+
+	channelGPDMA.ChannelNum = 0;
+	channelGPDMA.TransferSize = NUMERO_MUESTRAS;
+	channelGPDMA.SrcMemAddr = (uint32_t) (TABLA_DAC);
+	channelGPDMA.TransferType = GPDMA_TRANSFERTYPE_M2P;
+	channelGPDMA.DstConn = GPDMA_CONN_DAC;
+	channelGPDMA.DMALLI = 0;
+
+	GPDMA_Init();
+	GPDMA_Setup(&channelGPDMA);
+	GPDMA_ChannelCmd(0, ENABLE);
+
+}
+
+void configPINSEL (void) {
+	PINSEL_CFG_Type pinsel;
+	//Config P0.26 para salida analogica del DAC.
+	pinsel.Pinnum = PINSEL_PIN_26;
+	pinsel.Portnum = PINSEL_PORT_0;
+	pinsel.Funcnum = PINSEL_FUNC_2; 	//AOUT para DAC
+	pinsel.Pinmode = PINSEL_PINMODE_TRISTATE;
+	pinsel.OpenDrain = PINSEL_PINMODE_NORMAL;
+
+	PINSEL_ConfigPin(&pinsel);
+
+}
+void init(void){
+	configPINSEL();
+	configGPDMA();
+	configDAC();
+	configSYSTICK();
+}
+void crearLista(void){
+
+	TABLA_DAC[0] = 0;
+	for (int i = 1 ; i < RAMPA; i++)
+			TABLA_DAC[i] = TABLA_DAC[i - 1] + 10;		// Cargo los valores de la rampa
+	for (int i = RAMPA ; i < NUMERO_MUESTRAS ; i++)
+			TABLA_DAC[i] = 0;
+}
+void configSYSTICK(void){
+	SYSTICK_InternalInit(10);
+	SYSTICK_IntCmd(ENABLE);
+	SYSTICK_Cmd(ENABLE);
+}
+void SysTick_Handler(void){
+	SYSTICK_ClearCounterFlag();
+	IntStatus prueba = GPDMA_IntGetStatus(GPDMA_STAT_ENABLED_CH, 0);
+	IntStatus prueba2 = GPDMA_IntGetStatus(GPDMA_STAT_INT, 0);
+	IntStatus prueba3 = GPDMA_IntGetStatus(GPDMA_STAT_INTTC, 0);
 }
